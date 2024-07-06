bad33c9f2308cca08584450e9a0c487e34b91de8
Donaghpdmc <donaghpdmc@gmail.com>
2024-07-06 19:18:30 +0000

    Works as set for Lab 1

--- main.cpp
+++ main.cpp
@@ -1,11 +1,29 @@
 #include "mbed.h"
+#include <cstdio>
+/*
+a few words on what we learn on the lab
+/*
+
+InterruptIn bluebtn(BUTTON1);
+volatile int int_trig = 0;
+
+void bttnFall(){
+    int_trig = 1;
+}
+
 
 // main() runs in its own thread in the OS
 int main()
 {
+    bluebtn.fall(&bttnFall);
+
     // this is a program for lab 1 embedded systems module
     while (true) {
- 
+        if(int_trig == 1){
+            int_trig = 0;
+            printf("bttnFall \r\n");
+        
+        }
+    
     }
 }
-
